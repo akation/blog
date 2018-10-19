064071923a569eeeaa871fd3b3f17ba340b1a8c5
Andres Lora <andres@kation.com.do>
2018-10-19 15:44:09 -0400

    setup vue project

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <PostList></PostList>
+    </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList';
+
+export default {
+  components: {
+    PostList,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserLogin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-6 col-sm-6">
+        <form id="Login">
+          <div class="form-group">
+            <input name="email" type="email" class="form-control" placeholder="Email Address">
+          </div>
+          <div class="form-group">
+            <input name="password" type="password" class="form-control" placeholder="Password">
+          </div>
+          <button type="button" class="btn btn-primary">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
